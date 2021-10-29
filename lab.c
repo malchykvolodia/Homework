42db9c4c4a82d7cecbd25db0af922abb6761b237
malchykvolodia <volodamalk@gmail.com>
2021-10-29 15:58:47 +0300

    add reworked code of lab 3

--- lab.c
+++ lab.c
@@ -1,44 +1,45 @@
 #include <stdio.h>
-#include <math.h>
 
+void search_for_minimum_values (int array[], int number_of_elements)
+{
+    int min_1,min_2,position_of_1_min = 0;
+    min_1 = array[0];
+    min_2 = array[1];
+    for(int i = 0; i < number_of_elements; i++)
+    {
+        if(min_1 > array[i])
+        {
+            min_1 = array[i];
+            position_of_1_min = i;
+            break;
+        }
+
+    }
+    for(int i = 0; i <= (position_of_1_min - 1); i++)
+    {
+        if(min_2 > array[i])
+            min_2 = array[i];
+    }
+    for(int i = position_of_1_min+1; i < number_of_elements; i++)
+    {
+        if(min_2 > array[i])
+            min_2 = array[i];
+    }
+    printf("res= %d ", min_1 + min_2);
+    
+}
 int main()
 {
-    int n,i,a,b,v = 0;
+    int number_of_elements;
     printf("provide the number of items: ");
-    scanf("%d", &n);
-    int arr[n];
-    for(i = 0; i < n; i++)
+    scanf("%d", &number_of_elements);
+    int array[number_of_elements];
+    for(int i = 0; i < number_of_elements; i++)
     {
         printf("print element: ");
-        scanf("%d", &arr[i]);
+        scanf("%d", &array[i]);
     }
-    a = arr[0];
-    b = arr[1];
-    for(i = 0; i < n; i++)
-    {
-        if(a > arr[i])
-        {
-            a = arr[i];
-            v = i;
-        }
-        else
-            ;
-    }
-    for(i = 0; i <= (v-1); i++)
-    {
-        if(b > arr[i])
-            b = arr[i];
-        else
-            ;
-    }
-    for(i = v+1; i < n; i++)
-    {
-        if(b > arr[i])
-            b = arr[i];
-        else
-            ;
-    }
-    printf("res= %d ", a + b);
-
+    search_for_minimum_values(array, number_of_elements);
+    
     return 0;
 }
